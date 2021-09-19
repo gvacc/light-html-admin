80b498c5146ffbfc05e6676225490ef10cdb11b8
dertalian <ivadenlol@gmail.com>
2021-09-19 08:13:31 +0500

    добавил react, переписал логику на react с классами

--- app/src/api/create-page.php
+++ app/src/api/create-page.php
@@ -3,16 +3,23 @@ $_POST = array_merge($_POST,json_decode(file_get_contents('php://input'),true));
 $name = $_POST['file_name'];
 $response = [];
 
-$full_name = $name . '.html';
+if(!isset($name) || $name == '') {
+    header('HTTP/1.0 400 Bad Request');
+    $response['message'] = "Введите название файла!";
+    die();
+}
+
+$name_without_extension = preg_replace('/\\.[^.\\s]{3,4}$/', '', $name);
+$full_name = $name_without_extension . '.html';
 $new_file = '../../../../' . $full_name;
 
 try {
     if(!file_exists($new_file)) {
         fopen($new_file, 'w');
-        $response[] = $full_name; 
+        $response["file_name"] = $full_name; 
     
     } else {
-        header('HTTP/1.1 400 Bad Request');
+        header('HTTP/1.0 400 Bad Request');
         $response['message'] = "Файл {$full_name} уже существует!";
     }
 } catch (\Exception $e) {
---
+++ app/src/assets/js/components/editor/editor.js
@@ -0,0 +1,88 @@
+import React, {Component} from "react"
+import axios from 'axios'
+
+const validate_file_name = (() => {
+    const rg1=/^[^\\/:\*\?"<>\|]+$/; 
+    const rg2=/^\./; 
+    const rg3=/^(nul|prn|con|lpt[0-9]|com[0-9])(\.|$)/i; 
+    const rg4 = /^\s/
+
+    return (candidate) => {
+        const is_valid = rg1.test(candidate)
+                            &&!rg2.test(candidate)
+                            &&!rg3.test(candidate)
+                            &&!rg4.test(candidate)
+        if(is_valid) {
+            return candidate
+                    .replace(/(\.[^/.]+$)/, '')
+                    .replace(/\s/g, '')
+                    .toLowerCase();
+        } 
+        return false
+    }
+})()
+
+export default class Editor extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            pageList: [],
+            newPageName: '' //TODO: Поменять на file_name
+        }
+
+        this.createNewPage = this.createNewPage.bind(this)
+    }
+
+    componentDidMount() {
+        this.loadPageList()
+    }
+
+    async loadPageList() {
+        try {
+            const {data} = await axios.get('/admin/app/dist/api/')
+            this.setState({pageList: data})
+        } catch(e){
+            alert(e.response.data.message)
+        }
+        
+    }
+
+    async createNewPage() {
+        try {
+            const file_name = this.state.newPageName
+            const is_valid_name = validate_file_name(file_name)
+
+            if(!is_valid_name) {
+                alert('Некорректное название файла!')
+                return
+            }
+            const {data} = await axios.post('/admin/app/dist/api/create-page.php', {file_name})
+            this.setState({pageList: [data.file_name, ...this.state.pageList,]})
+
+        } catch(e){
+           alert(e.response.data.message)
+        }
+        
+    }
+
+    render() {
+        const {pageList} = this.state
+        const pages = pageList.map((page, idx) => {
+            return (
+                <h1 key={idx}>{page}</h1>
+            )
+        })
+        return (
+            <>
+                <input 
+                    type='text' 
+                    onChange={(e) => this.setState({newPageName: e.target.value})} 
+                    type="text"
+                />
+                <button onClick={this.createNewPage}>Создать файл</button>
+                {pages}
+            </>
+        )
+    }
+}
