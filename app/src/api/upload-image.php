bd8e3de381cbbe52435b11f4d4e0681d7b5ed097
dertalian <ivadenlol@gmail.com>
2021-09-21 09:47:59 +0500

    проверка, существует ли папка img

--- app/src/api/upload-image.php
+++ app/src/api/upload-image.php
@@ -4,6 +4,10 @@ if(file_exists($image) && is_uploaded_file($image)) {
 	$file_extension = explode('/', $_FILES['image']['type'])[1];
 	$file_name = uniqid() . '.' . $file_extension;
 	
+	if(!is_dir('../../../../img/')) {
+		mkdir('../../../../img/');
+	}
+
 	move_uploaded_file($image, '../../../../img/' . $file_name);
 
 	echo json_encode(array('src' => $file_name, 'message' => 'Изображение сохранено!'));
